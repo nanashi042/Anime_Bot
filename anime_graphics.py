00b2d17b67d53d937e487c295470e01ce125df8f
nanashi042 <ny1812@dseu.ac.in>
2023-01-23 14:27:33 +0530

    first commit

--- anime_graphics.py
+++ anime_graphics.py
@@ -5,12 +5,14 @@ class AnimeGraphic:
 
     def __init__(self):
         self.submit_button = None
+        self.submit_box = None
         self.screen = Tk()
 
 
     def app(self):
         self.screen.config(width=1280, height=720)
         self.submit_button = Button(text="Submit", width=20)
+        self.submit_box = Entry()
         self.submit_button.place(x=640, y=360)
         self.screen.mainloop()
 
